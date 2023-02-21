c54be6080161becd39fe81a4a242a7d03b04b5c5
hancyfancy <20492210+hancyfancy@users.noreply.github.com>
2023-02-21 21:56:59 +1100

    Add tests

---
+++ SampleGraphqlApp.Data.Test/Repository/StudentRepositoryTests.cs
@@ -0,0 +1,25 @@
+﻿using Newtonsoft.Json;
+using SampleGraphqlApp.Data.Interface.Models;
+using SampleGraphqlApp.Data.Interface.Repositories;
+
+namespace SampleGraphqlApp.Data.Test.Repository
+{
+    public class StudentRepositoryTests
+    {
+        private readonly IStudentRepository _studentRepository;
+
+        public StudentRepositoryTests(IStudentRepository studentRepository)
+        {
+            _studentRepository = studentRepository;
+        }
+
+        public async Task AllTest()
+        {
+            IEnumerable<Student>? students = await _studentRepository.All();
+
+            Assert.NotNull(students);
+
+            Assert.NotEqual("[]", JsonConvert.SerializeObject(students));
+        }
+    }
+}
---
+++ SampleGraphqlApp.Data.Test/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using SampleGraphqlApp.Data.Interface.Repositories;
+using SampleGraphqlApp.Data.Repositories;
+
+namespace SampleGraphqlApp.Data.Test
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddTransient<IStudentRepository, StudentRepository>();
+        }
+    }
+}

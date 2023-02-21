d8e5e01e9efb02943b7fbe03872b3dccd61d44af
hancyfancy <20492210+hancyfancy@users.noreply.github.com>
2023-02-21 21:25:58 +1100

    Add more student methods

--- SampleGraphqlApp.Data.Interface/Repositories/IStudentRepository.cs
+++ SampleGraphqlApp.Data.Interface/Repositories/IStudentRepository.cs
@@ -5,5 +5,14 @@ namespace SampleGraphqlApp.Data.Interface.Repositories
     public interface IStudentRepository
     {
         Task<IEnumerable<Student>?> All();
+
+        Task<Student?> ById(string id);
+
+        Task<IEnumerable<Student>?> ByProperties(
+            string email,
+            string firstName,
+            string lastName,
+            string collegeId
+        );
     }
 }
--- SampleGraphqlApp.Data/Repositories/StudentRepository.cs
+++ SampleGraphqlApp.Data/Repositories/StudentRepository.cs
@@ -12,14 +12,104 @@ namespace SampleGraphqlApp.Data.Repositories
             {
                 client.BaseAddress = new Uri("http://localhost:9000");
                 string query = $@"{{
-                                      students {{
+                                    students {{
                                         id
-                                        lastName
                                         firstName
+                                        lastName
+                                        email
                                         college {{
+                                          id
                                           name
                                           location
+                                          rating
                                           books {{
+                                            id
+                                            name
+                                            author
+                                          }}
+                                        }}
+                                    }}
+                                }}";
+                HttpContent content = new StringContent(query);
+                var result = await client.PostAsync("/graphql", content);
+                string resultContent = await result.Content.ReadAsStringAsync();
+                return JsonConvert.DeserializeObject<IEnumerable<Student>>(resultContent);
+            }
+        }
+
+        public async Task<Student?> ById(string id)
+        {
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("http://localhost:9000");
+                string query = $@"{{
+                                  student(id: ""{id}"") {{
+                                    firstName
+                                    lastName
+                                    email
+                                    college {{
+                                      id
+                                      name
+                                      location
+                                      rating
+                                      books {{
+                                        id
+                                        name
+                                        author
+                                      }}
+                                    }}
+                                  }}
+                                }}";
+                HttpContent content = new StringContent(query);
+                var result = await client.PostAsync("/graphql", content);
+                string resultContent = await result.Content.ReadAsStringAsync();
+                return JsonConvert.DeserializeObject<Student>(resultContent);
+            }
+        }
+
+        public async Task<IEnumerable<Student>?> ByProperties(
+            string email,
+            string firstName,
+            string lastName,
+            string collegeId
+        )
+        {
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("http://localhost:9000");
+
+                string queryEmail = string.IsNullOrWhiteSpace(email)
+                                    ? ""
+                                    : $@"email: {email}";
+                string queryFirstName = string.IsNullOrWhiteSpace(firstName)
+                                    ? ""
+                                    : $@"firstName: {firstName}";
+                string queryLastName = string.IsNullOrWhiteSpace(lastName)
+                                    ? ""
+                                    : $@"lastName: {lastName}";
+                string queryCollegeId = string.IsNullOrWhiteSpace(collegeId)
+                                    ? ""
+                                    : $@"collegeId: {collegeId}";
+
+                string query = $@"{{
+                                  studentsBy(search: {{
+                                      {queryEmail}
+                                      {queryFirstName}
+                                      {queryLastName}
+                                      {queryCollegeId}
+                                    }})
+                                    {{
+                                        id
+                                        firstName
+                                        lastName
+                                        email
+                                        college {{
+                                          id
+                                          name
+                                          location
+                                          rating
+                                          books {{
+                                            id
                                             name
                                             author
                                           }}

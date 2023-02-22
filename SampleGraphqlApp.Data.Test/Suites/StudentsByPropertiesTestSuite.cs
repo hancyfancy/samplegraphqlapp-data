02c37cda0c408fe1debf22d08fdddea505aeb869
hancyfancy <20492210+hancyfancy@users.noreply.github.com>
2023-02-22 15:10:42 +1100

    Create students by properties test

--- SampleGraphqlApp.Data.Test/Suites/StudentsByPropertiesTestSuite.cs
+++ SampleGraphqlApp.Data.Test/Suites/StudentsByPropertiesTestSuite.cs
@@ -11,7 +11,7 @@ namespace SampleGraphqlApp.Data.Test.Suites
     {
         public IEnumerator<object[]> GetEnumerator()
         {
-            //Non fields populated
+            //No fields populated
             yield return new object[] {
                 $@"[]",
                 "",
@@ -20,7 +20,7 @@ namespace SampleGraphqlApp.Data.Test.Suites
                 ""
             };
 
-            //Existent id
+            //Email populated
             yield return new object[] {
                 $@"[
 					{{
@@ -33,20 +33,23 @@ namespace SampleGraphqlApp.Data.Test.Suites
 							""id"": ""col-101"",
 							""name"": ""AMU"",
 							""location"": ""Uttar Pradesh"",
-							""rating"": 5,
+							""rating"": 5.0,
 							""books"": 
 							[
 								{{
 									""id"": ""bok-103"",
 									""name"": ""The Last Flair Bender"",
-									""author"": ""Manny Abuduo""
+									""author"": ""Manny Abuduo"",
+									""colleges"":null
 								}},
 								{{
 									""id"": ""bok-105"",
 									""name"": ""On the wild side"",
-									""author"": ""Harriett Osbyorne""
+									""author"": ""Harriett Osbyorne"",
+									""colleges"":null
 								}}
-							]
+							],
+							""students"":null
 						}}
 					}}
 				]",
@@ -54,6 +57,232 @@ namespace SampleGraphqlApp.Data.Test.Suites
                 "",
                 "",
                 ""
+            };
+
+            //FirstName populated
+            yield return new object[] {
+                $@"[
+					{{
+						""id"": ""S1001"",
+						""firstName"": ""Mohtashim"",
+						""lastName"": ""Mohammad"",
+						""email"": ""mohtashim.mohammad@tutorialpoint.org"",
+						""college"": 
+						{{
+							""id"": ""col-102"",
+							""name"": ""CUSAT"",
+							""location"": ""Kerala"",
+							""rating"": 4.5,
+							""books"": 
+							[
+								{{
+									""id"": ""bok-102"",
+									""name"": ""Knight and Bay"",
+									""author"": ""Zon Drekka"",
+									""colleges"":null
+								}},
+								{{
+									""id"": ""bok-105"",
+									""name"": ""On the wild side"",
+									""author"": ""Harriett Osbyorne"",
+									""colleges"":null
+								}},
+								{{
+									""id"": ""bok-106"",
+									""name"": ""Empty Space"",
+									""author"": ""Xelvudi Trilchanov"",
+									""colleges"":null
+								}}
+							],
+							""students"":null
+						}}
+					}}
+				]",
+                "",
+                "Mohtashim",
+                "",
+                ""
+            };
+
+            //LastName populated
+            yield return new object[] {
+                $@"[
+					{{
+						""id"": ""S1002"",
+						""firstName"": ""Kannan"",
+						""lastName"": ""Sudhakaran"",
+						""email"": ""kannan.sudhakaran@tutorialpoint.org"",
+						""college"": 
+						{{
+							""id"": ""col-101"",
+							""name"": ""AMU"",
+							""location"": ""Uttar Pradesh"",
+							""rating"": 5.0,
+							""books"": 
+							[
+								{{
+									""id"": ""bok-103"",
+									""name"": ""The Last Flair Bender"",
+									""author"": ""Manny Abuduo"",
+									""colleges"":null
+								}},
+								{{
+									""id"": ""bok-105"",
+									""name"": ""On the wild side"",
+									""author"": ""Harriett Osbyorne"",
+									""colleges"":null
+								}}
+							],
+							""students"":null
+						}}
+					}}
+				]",
+                "",
+                "",
+                "Sudhakaran",
+                ""
+            };
+
+            //CollegeId populated
+            yield return new object[] {
+                $@"[
+					{{
+						""id"": ""S1002"",
+						""firstName"": ""Kannan"",
+						""lastName"": ""Sudhakaran"",
+						""email"": ""kannan.sudhakaran@tutorialpoint.org"",
+						""college"": 
+						{{
+							""id"": ""col-101"",
+							""name"": ""AMU"",
+							""location"": ""Uttar Pradesh"",
+							""rating"": 5.0,
+							""books"": 
+							[
+								{{
+									""id"": ""bok-103"",
+									""name"": ""The Last Flair Bender"",
+									""author"": ""Manny Abuduo"",
+									""colleges"":null
+								}},
+								{{
+									""id"": ""bok-105"",
+									""name"": ""On the wild side"",
+									""author"": ""Harriett Osbyorne"",
+									""colleges"":null
+								}}
+							],
+							""students"":null
+						}}
+					}},
+					{{
+						""id"": ""S1003"",
+						""firstName"": ""Kiran"",
+						""lastName"": ""Panigrahi"",
+						""email"": ""kiran.panigrahi@tutorialpoint.org"",
+						""college"": 
+						{{
+							""id"": ""col-101"",
+							""name"": ""AMU"",
+							""location"": ""Uttar Pradesh"",
+							""rating"": 5.0,
+							""books"": 
+							[
+								{{
+									""id"": ""bok-103"",
+									""name"": ""The Last Flair Bender"",
+									""author"": ""Manny Abuduo"",
+									""colleges"":null
+								}},
+								{{
+									""id"": ""bok-105"",
+									""name"": ""On the wild side"",
+									""author"": ""Harriett Osbyorne"",
+									""colleges"":null
+								}}
+							],
+							""students"":null
+						}}
+					}}
+				]",
+                "",
+                "",
+                "",
+                "col-101"
+            };
+
+            //Fields for different records populated
+            yield return new object[] {
+                $@"[
+					{{
+						""id"": ""S1003"",
+						""firstName"": ""Kiran"",
+						""lastName"": ""Panigrahi"",
+						""email"": ""kiran.panigrahi@tutorialpoint.org"",
+						""college"": 
+						{{
+							""id"": ""col-101"",
+							""name"": ""AMU"",
+							""location"": ""Uttar Pradesh"",
+							""rating"": 5.0,
+							""books"": 
+							[
+								{{
+									""id"": ""bok-103"",
+									""name"": ""The Last Flair Bender"",
+									""author"": ""Manny Abuduo"",
+									""colleges"":null
+								}},
+								{{
+									""id"": ""bok-105"",
+									""name"": ""On the wild side"",
+									""author"": ""Harriett Osbyorne"",
+									""colleges"":null
+								}}
+							],
+							""students"":null
+						}}
+					}},
+					{{
+						""id"": ""S1001"",
+						""firstName"": ""Mohtashim"",
+						""lastName"": ""Mohammad"",
+						""email"": ""mohtashim.mohammad@tutorialpoint.org"",
+						""college"": 
+						{{
+							""id"": ""col-102"",
+							""name"": ""CUSAT"",
+							""location"": ""Kerala"",
+							""rating"": 4.5,
+							""books"": 
+							[
+								{{
+									""id"": ""bok-102"",
+									""name"": ""Knight and Bay"",
+									""author"": ""Zon Drekka"",
+									""colleges"":null
+								}},
+								{{
+									""id"": ""bok-105"",
+									""name"": ""On the wild side"",
+									""author"": ""Harriett Osbyorne"",
+									""colleges"":null
+								}},
+								{{
+									""id"": ""bok-106"",
+									""name"": ""Empty Space"",
+									""author"": ""Xelvudi Trilchanov"",
+									""colleges"":null
+								}}
+							],
+							""students"":null
+						}}
+					}}
+				]",
+                "kiran.panigrahi@tutorialpoint.org",
+                "",
+                "",
+                "col-102"
             };
         }
 
